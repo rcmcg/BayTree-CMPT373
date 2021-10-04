426412aa53d89b011898ae907bc2c7f039af20a0
Reece McGowan <reecem@sfu.ca>
2021-10-04 07:54:33 -0700

    Add menteeId field and install axios.
    Adding menteeId field to test sending requests to backend. In the future, will implement a drop-down which displays list of available mentees that the mentor is currently mentoring.

--- user-frontend/src/components/SessionInformation.tsx
+++ user-frontend/src/components/SessionInformation.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
 
+const axios = require('axios').default;
+
+class SelectMentee extends React.Component {
+    render () {
+        return (
+            <div>
+                <label form="selectMenteeId">Mentee id</label>
+                <input type="number" id="selectMenteeId" name="menteeId" required/>
+            </div>
+        )
+    }
+}
+
 class ClockIn extends React.Component {
     render() {
         return (
             <div>
                 <label form="clockInId">Session clock in date and time</label>
-                <input type="datetime-local" id="clockInId" name="clockInName" required/>
+                <input type="datetime-local" id="clockInId" name="clockInTimeLocal" required/>
             </div>
         );
     }
@@ -16,7 +29,7 @@ class ClockOut extends React.Component {
         return (
             <div>
                 <label form="clockOutId">Session clock out date and time</label>
-                <input type="datetime-local" id="clockOutId" name="clockOutName" required/>
+                <input type="datetime-local" id="clockOutId" name="clockOutTimeLocal" required/>
             </div>
         );
     }
@@ -27,7 +40,7 @@ class SessionNotes extends React.Component {
         return (
             <div>
                 <label form="sessionNotes">Session notes</label>
-                <input type="text" id="sessionNotesId" name="sessionNotesForm" required/>
+                <input type="text" id="sessionNotesId" name="sessionNotes"/>
             </div>
         )
     }
@@ -47,7 +60,8 @@ export class SessionInformation extends React.Component {
     render () {
         return (
             <main>
-                <form>
+                <form action={"http://localhost:8080/session/add"} method={"post"}>
+                    <SelectMentee />
                     <ClockIn />
                     <ClockOut />
                     <SessionNotes />
