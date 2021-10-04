0eeebabc37d69aa4a26d0d06d28b17ef766bfa46
rchawla <rchawla@sfu.ca>
2021-10-04 03:54:07 -0700

    Added model for user authentication

---
+++ api/baytree_mentoring/src/main/java/com/baytree_mentoring/baytree_mentoring/models/Authentication.java
@@ -0,0 +1,40 @@
+package com.baytree_mentoring.baytree_mentoring.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Authentication {
+    @Id
+    private String username;
+
+    private String password;
+
+    public Authentication(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
