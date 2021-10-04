54178e6c2c30dc22818cca48b8bb88b58efa4d27
spadalka <sergii_padalka@sfu.ca>
2021-10-04 10:09:53 -0700

    Create basic dynamic table with mock data

--- admin-frontend/src/pages/Mentors/Mentors.tsx
+++ admin-frontend/src/pages/Mentors/Mentors.tsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./Styles/Mentors.css";
+import data from "./mockdata.json";
+
+interface IState {
+  mentors: {
+    Username: string;
+    Name: string;
+    Age: number;
+    Program: string;
+    StartDate: Date;
+    DailySessionsOutstanding: number;
+    MonthlyReportsOutstanding: number;
+  }[];
+}
 
 function Mentors() {
+  const [mentors, setMentors] = useState<IState["mentors"]>(data);
+
   return (
-    <div className='mentors'>
+    <div className="mentors">
       <h1>Mentors</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Program</th>
+            <th>Start Date</th>
+            <th>Daily Sessions Outstanding</th>
+            <th>Monthly Reports Outstanding</th>
+          </tr>
+        </thead>
+        <tbody>
+          {mentors.map((mentor) => (
+            <tr>
+              <td>{mentor.Username}</td>
+              <td>{mentor.Name}</td>
+              <td>{mentor.Age}</td>
+              <td>{mentor.Program}</td>
+              <td>{mentor.StartDate}</td>
+              <td>{mentor.DailySessionsOutstanding}</td>
+              <td>{mentor.MonthlyReportsOutstanding}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
