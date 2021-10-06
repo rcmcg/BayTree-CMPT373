03f14aab3f8398ccf8e94aba3e257cfa6ea0cdbc
Steven Tran <sttran@sfu.ca>
2021-10-05 17:16:49 -0700

    Refactored Mentor.tsx to remove duplicate mappings on JSON input

--- admin-frontend/src/pages/Mentors/Mentor.tsx
+++ admin-frontend/src/pages/Mentors/Mentor.tsx
@@ -3,82 +3,59 @@ import 'bootstrap/dist/css/bootstrap.css';
 import {Tabs, Tab} from 'react-bootstrap';
 import './styles/Mentors.css'
 
-// import Tabs from './components/Tabs/Tabs';
-// import Tab from './components/Tabs/Tab';
 import { mentorData } from '../../assets/dummy-data/DummyMentors'; 
 import { mentor1Data } from '../../assets/dummy-data/DummyMentor1'; 
 function Mentor() {
-  return (
-    <div className='mentor'>
-        <h1>Mentor</h1>
-    
-        <Tabs defaultActiveKey="sessions" className = 'tab'>
-            <Tab eventKey="sessions" title="Sessions">
-                {mentor1Data.map((data, key) => {
-                    return (
-                        <div key={key}>
-                            {data.sessions.map((session, sessionKey) => {
-                                return (
-                                    <div key={sessionKey}>{
-                                        <div>
-                                            <h2>{session.date}</h2>
-                                            <p>Mentee: {session.mentee}</p>
-                                            <p>Notes: {session.notes}</p>
-                                        </div>}
-                                    </div>
-                                )
-                            })}
-                        </div>
-                    );
-                })}
-            </Tab>
-            <Tab eventKey="questionnaire" title="Questionnaire">
-                {mentor1Data.map((data, key) => {
-                    return (
-                        <div key={key}>
-                            {data.questionnaires.map((questionnaire, questionnaireKey) => {
-                                return (
-                                    <div key={questionnaireKey}>{
-                                        <div>
-                                            <h2>{questionnaire.month}</h2>
-                                            <h3>Mentee: {questionnaire.mentee}</h3>
-                                            <p><strong>Q1: {questionnaire.q1}</strong> {questionnaire.a1}</p>
-                                            <p><strong>Q2: {questionnaire.q2}</strong> {questionnaire.a2}</p>
-                                        </div>}
-                                    </div>
-                                )
-                            })}
-                        </div>
-                    );
-                })}
-            </Tab>
-            <Tab eventKey="info" title="Personal Information">
-                {mentor1Data.map((data, key) => {
-                    return (
-                        <div key={key}>{
-                            <div>
-                                <h2>{data.name}</h2>
-                                <p> ID: {data.id} </p>                            
-                            </div>}
-                        </div>
-                    );
-                })}
-            </Tab>
-        </Tabs>
-
-      {/* <Tabs>
-        <Tab title = "Sessions">
-
-        </Tab>
-        <Tab title = "Questionnaires">
-
-        </Tab>
-        <Tab title = "Personal Information">
-
-        </Tab>
-      </Tabs> */}
-    </div>
-  );
+    return (
+        <div className='mentor'>
+            <h1>Mentor</h1>
+            {mentor1Data.map((data, key) => {
+                return (
+                    <Tabs defaultActiveKey="sessions" className = 'tab'>
+                        <Tab eventKey="sessions" title="Sessions">
+                            <div key={key}>
+                                {data.sessions.map((session, sessionKey) => {
+                                    return (
+                                        <div key={sessionKey}>{
+                                            <div>
+                                                <h2>{session.date}</h2>
+                                                <p><strong>Mentee:</strong> {session.mentee}</p>
+                                                <p><strong>Notes:</strong> {session.notes}</p>
+                                            </div>}
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </Tab>
+                        <Tab eventKey="questionnaire" title="Questionnaire">
+                            <div key={key}>
+                                {data.questionnaires.map((questionnaire, questionnaireKey) => {
+                                    return (
+                                        <div key={questionnaireKey}>{
+                                            <div>
+                                                <h2>{questionnaire.month}</h2>
+                                                <h3>Mentee: {questionnaire.mentee}</h3>
+                                                <p><strong>Q1: {questionnaire.q1}</strong> {questionnaire.a1}</p>
+                                                <p><strong>Q2: {questionnaire.q2}</strong> {questionnaire.a2}</p>
+                                            </div>}
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </Tab>
+                        <Tab eventKey="info" title="Personal Information">
+                            <div key={key}>{
+                                <div>
+                                    <h2>{data.name}</h2>
+                                    <p><strong>ID:</strong> {data.id} </p>                            
+                                </div>}
+                            </div>
+                        </Tab>
+                    </Tabs>
+                );
+            })}
+        </div>
+    );
 }
 
 export default Mentor;
