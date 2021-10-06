38df3ff50d6a10390b66b5db3d892aa5b1bd2ba4
rchawla <rchawla@sfu.ca>
2021-10-05 20:59:55 -0700

    removed the getters from the authentication class

--- api/baytree_mentoring/src/main/java/com/baytree_mentoring/baytree_mentoring/models/Authentication.java
+++ api/baytree_mentoring/src/main/java/com/baytree_mentoring/baytree_mentoring/models/Authentication.java
@@ -1,5 +1,6 @@
 package com.baytree_mentoring.baytree_mentoring.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,31 +11,15 @@ import javax.persistence.Id;
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
+@AllArgsConstructor
 public class Authentication {
     @Id
     private String username;
 
     private String password;
 
-    public Authentication(String username, String password){
-        this.username = username;
-        this.password = password;
-    }
 
-    public String getUsername() {
-        return username;
-    }
+    public Authentication() {
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
