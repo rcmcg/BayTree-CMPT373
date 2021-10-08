8e932f12f405168b747669fde328e60efdea5a2d
Steven Tran <sttran@sfu.ca>
2021-10-08 00:24:20 -0700

    Changed Mentor.tsx to use data from Views, removed dummy data that was not in .json format, removed useless code from Mentors.tsx

--- admin-frontend/src/pages/Mentors/Mentor.tsx
+++ admin-frontend/src/pages/Mentors/Mentor.tsx
@@ -3,57 +3,60 @@ import 'bootstrap/dist/css/bootstrap.css';
 import {Tabs, Tab} from 'react-bootstrap';
 import './styles/Mentors.css'
 
-import { mentorData } from '../../assets/dummy-data/DummyMentors'; 
-import { mentor1Data } from '../../assets/dummy-data/DummyMentor1'; 
+
+import mentor from '../../assets/dummy-data/mentor28/Mentor.json';
+import sessionsData from '../../assets/dummy-data/mentor28/Sessions.json';
+import questionnairesData from '../../assets/dummy-data/mentor28/Questionnaires.json';
+
 function Mentor() {
     return (
         <div className='mentor'>
             <h1>Mentor</h1>
-            {mentor1Data.map((data, key) => {
-                return (
-                    <Tabs defaultActiveKey="sessions" className = 'tab'>
-                        <Tab eventKey="sessions" title="Sessions">
-                            <div key={key}>
-                                {data.sessions.map((session, sessionKey) => {
-                                    return (
-                                        <div key={sessionKey}>{
-                                            <div>
-                                                <h2>{session.date}</h2>
-                                                <p><strong>Mentee:</strong> {session.mentee}</p>
-                                                <p><strong>Notes:</strong> {session.notes}</p>
-                                            </div>}
+                <Tabs defaultActiveKey="sessions" className = 'tab'>
+                    <Tab eventKey="sessions" title="Sessions">
+                        {Object.values(sessionsData).map((sessions) => {
+                            return(
+                                Object.values(sessions).map((session) => {
+                                    const date = new Date(session.StartDate);
+                                    return(
+                                        <div>
+                                            <h2>{date.toDateString()}</h2>
+                                            <h3>{session.Title}</h3>
+                                            <p><strong>Start Time: </strong> {date.toLocaleTimeString("en-US")}</p>
+                                            <p><strong>Duration: </strong> {session.Duration}</p>
+                                            <p><strong>Attendance: </strong> {session.Status}</p>
                                         </div>
                                     )
-                                })}
-                            </div>
-                        </Tab>
-                        <Tab eventKey="questionnaire" title="Questionnaire">
-                            <div key={key}>
-                                {data.questionnaires.map((questionnaire, questionnaireKey) => {
-                                    return (
-                                        <div key={questionnaireKey}>{
-                                            <div>
-                                                <h2>{questionnaire.month}</h2>
-                                                <h3>Mentee: {questionnaire.mentee}</h3>
-                                                <p><strong>Q1: {questionnaire.q1}</strong> {questionnaire.a1}</p>
-                                                <p><strong>Q2: {questionnaire.q2}</strong> {questionnaire.a2}</p>
-                                            </div>}
-                                        </div>
-                                    )
-                                })}
-                            </div>
-                        </Tab>
-                        <Tab eventKey="info" title="Personal Information">
-                            <div key={key}>{
+                                })
+                            )
+                        })}
+                    </Tab>
+                    <Tab eventKey="questionnaire" title="Questionnaire">
+                        {Object.values(questionnairesData).map((questionnaire) => {
+                            return(
                                 <div>
-                                    <h2>{data.name}</h2>
-                                    <p><strong>ID:</strong> {data.id} </p>                            
-                                </div>}
-                            </div>
-                        </Tab>
-                    </Tabs>
-                );
-            })}
+                                    <h2>{questionnaire.Questionnaire}</h2>
+                                    <p><strong>Questionnaire ID: </strong> {questionnaire.QuestionnaireID}</p>
+                                    <p><strong>Answers ID: </strong> {questionnaire.AnswerSetID}</p>
+                                </div>
+                            );
+                        })}
+                    </Tab>
+                    <Tab eventKey="info" title="Personal Information">
+                        <div className = "info">
+                            <h2>{mentor.Surname}, {mentor.Forename}</h2>
+                            <p><strong>Volunteer Status:</strong> {mentor.VolunteerStatus_V_1} </p>
+                            <p><strong>Volunteer Role:</strong> {mentor.Volunteerrole_V_34.replaceAll("|", ", ")} </p>                 
+                            <p><strong>Start Date:</strong> {mentor.Startdate_V_37} </p>  
+                            <p><strong>Age:</strong> {Math.floor(Number(mentor.Age)/365)} </p>  
+                            <p><strong>Gender:</strong> {mentor.Gender} </p> 
+                            <p><strong>Email:</strong> {mentor.Email} </p>
+                            <p><strong>Primary Language:</strong> {mentor.Whatisyourfirstlanguage_V_19} </p>
+                            <p><strong>Other Languages:</strong> {mentor.Anyotherlanguages_V_28.replaceAll("|", ", ")} </p>
+                        </div>
+                    </Tab>
+                </Tabs>
+            );
         </div>
     );
 }
--- admin-frontend/src/pages/Mentors/Mentors.tsx
+++ admin-frontend/src/pages/Mentors/Mentors.tsx
@@ -3,13 +3,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './styles/Mentors.css'
 import {Link} from 'react-router-dom';
 
-import { mentorData } from '../../assets/dummy-data/DummyMentors'; 
-
 function Mentors() {
   return (
     <div className='mentors'>
       <h1>Mentors</h1>
-      <Link to= '/mentor'>
+        <Link to= '/mentor'>
         <p>test</p>
       </Link>
     </div>
