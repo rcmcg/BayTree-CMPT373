fa3144637333e46f83718232d85ed3e892c1cf97
Reece McGowan <reecem@sfu.ca>
2021-10-10 23:02:57 -0930

    Create new session with Views API.
    Send a completed session form from the front end to the Views database

--- api/baytree_mentoring/src/main/java/com/baytree_mentoring/baytree_mentoring/services/SessionService.java
+++ api/baytree_mentoring/src/main/java/com/baytree_mentoring/baytree_mentoring/services/SessionService.java
@@ -2,9 +2,16 @@ package com.baytree_mentoring.baytree_mentoring.services;
 
 import com.baytree_mentoring.baytree_mentoring.models.Session;
 import com.baytree_mentoring.baytree_mentoring.repositories.SessionRepository;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class SessionService {
@@ -35,6 +42,41 @@ public class SessionService {
 
     public void deleteSession(long mentoringSessionId) {
         sessionRepository.deleteById(mentoringSessionId);
+    }
+
+    public void sendCompletedSessionFormToViews(Session ses) {
+        // hard code in 28 (Mercury Team) and 2 for Venue ID, for now
+        Unirest.setTimeouts(0,0);
+        String body = formatSessionUploadJson(ses.getClockInTimeLocal(), ses.getClockOutTimeLocal(), "28", "2");
+        try {
+            // hardcode upload to our test group for now
+            HttpResponse<String> response = Unirest.post("https://app.viewsapp.net/api/restful/work/sessiongroups/10/sessions")
+                    .header("Content-Type", "application/json")
+                    .basicAuth("group.mercury", "Mercury!$%12")
+                    .body(body)
+                    .asString();
+            System.out.println(response.getBody().toString());
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String formatSessionUploadJson(String clockInTime, String clockOutTime, String leadStaff, String venueId) {
+        // convert clockInTime 2021-10-10 22:14:00 -0930 YYYY-MM-DD
+        Date clockInDate = null;
+        Date clockOutDate = null;
+        try {
+            clockInDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(clockInTime);
+            clockOutDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(clockOutTime);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        String startDate = clockInTime.substring(0,10);
+        String startTime = clockInDate.toString().substring(11,19);
+        long durationInMilliseconds = Math.abs(clockOutDate.getTime() - clockInDate.getTime());
+        long duration = TimeUnit.HOURS.convert(durationInMilliseconds, TimeUnit.MILLISECONDS);
+        String body = "{\r\n   \"StartDate\": \""+startDate+"\",\r\n   \"StartTime\": \""+startTime+"\",\r\n   \"Duration\": \""+duration+"\",\r\n   \"LeadStaff\": \""+leadStaff+"\",\r\n   \"VenueID\": \""+venueId+"\"\r\n}";
+        return body;
     }
 
 }
