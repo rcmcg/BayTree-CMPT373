3ee08c40a57e90ddef214c841d3e09e87121b6b0
rchawla <rchawla@sfu.ca>
2021-10-24 00:46:11 -0700

    Added the user class

---
+++ api/baytree_mentoring/src/main/java/com/baytree_mentoring/baytree_mentoring/models/Users.java
@@ -0,0 +1,15 @@
+package com.baytree_mentoring.baytree_mentoring.models;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+
+public class Users {
+
+
+
+}
