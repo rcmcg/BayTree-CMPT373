0d4508558692fd3a2147247a44d222b51ad69fb8
rchawla <rchawla@sfu.ca>
2021-10-24 22:41:05 -0700

    Added constructors, getters, required fields for entities in User class

--- api/baytree_mentoring/src/main/java/com/baytree_mentoring/baytree_mentoring/models/User.java
+++ api/baytree_mentoring/src/main/java/com/baytree_mentoring/baytree_mentoring/models/User.java
@@ -2,19 +2,52 @@ package com.baytree_mentoring.baytree_mentoring.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    public User(int viewsId, String firstName, String lastNAme, String email, String status) {
+        this.viewsId = viewsId;
+        this.firstName = firstName;
+        this.lastNAme = lastNAme;
+        this.email = email;
+        this.status = status;
+    }
+
     @Id
     private int viewsId;
 
-    public User() {
+    String firstName;
 
+    String lastNAme;
+
+    String email;
+
+    String status;
+
+    public int getViewsId() {
+        return viewsId;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastNAme() {
+        return lastNAme;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
