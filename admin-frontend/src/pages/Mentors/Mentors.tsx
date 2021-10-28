46e25d3aa0a6033479a3d7266577cf6878ddfa05
spadalka <spadalka@sfu.ca>
2021-10-28 09:49:34 -0700

    Complete pagination

--- admin-frontend/src/pages/Mentors/Mentors.tsx
+++ admin-frontend/src/pages/Mentors/Mentors.tsx
@@ -1,5 +1,5 @@
-import React, { useMemo } from "react";
-import { useTable, useFilters, useSortBy } from "react-table";
+import React, { useState, useMemo } from "react";
+import { useTable, useFilters, useSortBy, usePagination } from "react-table";
 import DATA from "./mockdata.json";
 import { COLUMNS } from "./Columns";
 import CustomDatePicker from "./customDatePicker";
@@ -9,20 +9,35 @@ const Mentors = () => {
   const columns = useMemo(() => COLUMNS, []);
   const data = useMemo(() => DATA, []);
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable(
-      {
-        columns,
-        data,
-      },
-      useFilters,
-      useSortBy
-    );
-
-  const [startDate, setStartDate] = React.useState(
-    new Date("2010-01-01T00:00:00")
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    nextPage,
+    previousPage,
+    canNextPage,
+    canPreviousPage,
+    pageOptions,
+    gotoPage,
+    pageCount,
+    setPageSize,
+    state,
+    prepareRow,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useFilters,
+    useSortBy,
+    usePagination
   );
-  const [finishDate, setFinishDate] = React.useState(
+
+  const { pageIndex, pageSize } = state;
+
+  const [startDate, setStartDate] = useState(new Date("2010-01-01T00:00:00"));
+  const [finishDate, setFinishDate] = useState(
     new Date(moment().format("YYYY-MM-DDTHH:mm:ss"))
   );
 
@@ -55,7 +70,7 @@ const Mentors = () => {
           ))}
         </thead>
         <tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
+          {page.map((row) => {
             prepareRow(row);
             return (
               <tr {...row.getRowProps()}>
@@ -69,6 +84,47 @@ const Mentors = () => {
           })}
         </tbody>
       </table>
+      <div>
+        <select
+          value={pageSize}
+          onChange={(e) => setPageSize(Number(e.target.value))}
+        >
+          {[10, 25, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+        <span>
+          Page {pageIndex + 1} of {pageOptions.length}
+        </span>
+        <span>
+          | Go to page:{" "}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const pageNumber = e.target.value
+                ? Number(e.target.value) - 1
+                : 0;
+              gotoPage(pageNumber);
+            }}
+            style={{ width: "50px" }}
+          />
+        </span>{" "}
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>{" "}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Prev
+        </button>{" "}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next
+        </button>{" "}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {">>"}
+        </button>{" "}
+      </div>
     </>
   );
 };
--- admin-frontend/src/pages/Mentors/customDatePicker.tsx
+++ admin-frontend/src/pages/Mentors/customDatePicker.tsx
@@ -11,9 +11,11 @@ const CustomDatePicker = (props: any) => {
 
   const handleDateChangeStart = (date: any) => {
     setStartDate(date);
+    console.log(date);
   };
   const handleDateChangeFinish = (date: any) => {
     setFinishDate(date);
+    console.log(date);
   };
 
   return (
@@ -37,7 +39,7 @@ const CustomDatePicker = (props: any) => {
             id="date-picker-dialog-finish"
             label={<span style={{ opacity: 0.6 }}>Finish Date</span>}
             format="dd/MM/yyyy"
-            // clearable
+            clearable
             value={finishDate}
             onChange={handleDateChangeFinish}
             KeyboardButtonProps={{
