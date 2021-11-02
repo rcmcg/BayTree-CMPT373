e1684a8518044b136722aca786f8aac123df5c5c
rchawla <rchawla@sfu.ca>
2021-11-02 14:36:32 -0700

    Made async API call to the springboot

--- admin-frontend/src/pages/Resources/Resources.tsx
+++ admin-frontend/src/pages/Resources/Resources.tsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getResourcesList } from './Resources.api';
+
+// interface ResourcesProps {
+//   Resource: any
+
+// }
+
+// const Resources: React.FC<ResourcesProps> = ({Resource}) => {
+
+// }
 
 function Resources() {
+  useEffect(() => {
+    const fetchResourceList = async () => {
+      const resourceList = await getResourcesList()
+      console.log(resourceList)
+    }
+    fetchResourceList()
+  },[])
   return (
     <div className='resources'>
       <h1>Add Resources</h1>
