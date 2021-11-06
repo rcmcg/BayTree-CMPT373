48262f8201503c631aafc4a8c58abaa463d2c91a
Pruthviraj Patel <pruthviraj_patel@sfu.ca>
2021-11-05 19:44:13 -0700

    Fixed the dashboard to resemble that picture sent in discord

--- user-frontend/src/components/NavigationBar.tsx
+++ user-frontend/src/components/NavigationBar.tsx
@@ -33,17 +33,17 @@ const NavigationBar = () => {
 
                     <li className={"nav-item"}>
                         <Link activeClassName={"active"} exact to={'/'} className="nav-links" onClick={closeMobileMenu}>
-                            Dashboard
+                            <a>Dashboard</a>
                         </Link>
                     </li>
                     <li className={"nav-item"}>
                         <Link to={'/resources'} className="nav-links" onClick={closeMobileMenu}>
-                            Resources
+                            <a>Resources</a>
                         </Link>
                     </li>
                     <li className={"nav-item"}>
                         <Link to={'/profile'} className="nav-links" onClick={closeMobileMenu}>
-                            Profile
+                            <a>Profile</a>
                         </Link>
                     </li>
 
