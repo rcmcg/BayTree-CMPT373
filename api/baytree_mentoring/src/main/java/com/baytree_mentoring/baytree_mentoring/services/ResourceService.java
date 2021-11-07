3e21a05a5ced15a2a36753137611d6cab676d3e2
rchawla <rchawla@sfu.ca>
2021-11-07 15:33:14 -0800

    added the add and getResource funtions to intract with the repository

--- api/baytree_mentoring/src/main/java/com/baytree_mentoring/baytree_mentoring/services/ResourceService.java
+++ api/baytree_mentoring/src/main/java/com/baytree_mentoring/baytree_mentoring/services/ResourceService.java
@@ -1,8 +1,25 @@
 package com.baytree_mentoring.baytree_mentoring.services;
 
+import com.baytree_mentoring.baytree_mentoring.models.Resource;
+import com.baytree_mentoring.baytree_mentoring.repositories.ResourceRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ResourceService {
 
+    private final ResourceRepository resourceRepository;
+
+    public ResourceService(ResourceRepository resourceRepository) {
+        this.resourceRepository = resourceRepository;
+    }
+
+    public void AddResourceService(Resource res) {
+        resourceRepository.save(res);
+    }
+
+    public List<Resource> getAllResources() {
+        return resourceRepository.findAll();
+    }
 }
