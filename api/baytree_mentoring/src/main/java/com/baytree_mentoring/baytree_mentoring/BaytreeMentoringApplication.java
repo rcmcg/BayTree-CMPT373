c90dfcaf5eb50e6f40b03550c1a30a4e6fc1801e
OlegShoykhedenko <70909936+OlegShoykhedenko@users.noreply.github.com>
2021-11-07 18:58:17 -0800

    fixed problem with starting the application

--- api/baytree_mentoring/src/main/java/com/baytree_mentoring/baytree_mentoring/BaytreeMentoringApplication.java
+++ api/baytree_mentoring/src/main/java/com/baytree_mentoring/baytree_mentoring/BaytreeMentoringApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 
@@ -17,6 +19,11 @@ public class BaytreeMentoringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BaytreeMentoringApplication.class, args);
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 	@Bean
