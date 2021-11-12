a295561e0acfbfdb11794f2efb9157fef7a5ef8f
rchawla <rchawla@sfu.ca>
2021-11-11 17:50:48 -0800

    Changed the type for the API request

--- admin-frontend/src/pages/Resources/Resources.api.ts
+++ admin-frontend/src/pages/Resources/Resources.api.ts
@@ -1,7 +1,8 @@
 import Api from "./api";
+import { Resource } from "./types";
 
 const getResourcesList = async() =>  {
-    const response = await Api.get('/user/get/mentors/all')
+    const response = await Api.get<Resource[]>('/user/get/mentors/all')
     return response.data;
 }
 
