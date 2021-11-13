05db32e86d067b0fe00f69902c04de546ceade5e
Pruthviraj Patel <pruthviraj_patel@sfu.ca>
2021-11-12 17:24:45 -0800

    Removed an endpoint from controller where it didn't belong

--- api/baytree_mentoring/src/main/java/com/baytree_mentoring/baytree_mentoring/controllers/SessionController.java
+++ api/baytree_mentoring/src/main/java/com/baytree_mentoring/baytree_mentoring/controllers/SessionController.java
@@ -2,16 +2,11 @@ package com.baytree_mentoring.baytree_mentoring.controllers;
 
 import com.baytree_mentoring.baytree_mentoring.exceptions.FailedSessionAddingException;
 import com.baytree_mentoring.baytree_mentoring.models.Session;
-import com.baytree_mentoring.baytree_mentoring.models.ViewsMentee;
 import com.baytree_mentoring.baytree_mentoring.services.MenteeService;
 import com.baytree_mentoring.baytree_mentoring.services.SessionService;
 import com.baytree_mentoring.baytree_mentoring.util.ViewsUnirest;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.exceptions.UnirestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 public class SessionController {
@@ -38,49 +33,6 @@ public class SessionController {
             String error = "Failed to upload session to Views database";
             throw new FailedSessionAddingException(error);
         }
-    }
-
-    // Todo: refactor this method and related changes in this commit
-    @ResponseStatus(HttpStatus.OK)
-    @CrossOrigin(origins = "http://localhost:3000")
-    @GetMapping("/test")
-    private String test() throws UnirestException {
-////        System.out.println("inside test()");
-//        // return "WHATEVER THE RESPONSE IS";
-        String URL = "https://app.viewsapp.net/api/restful/contacts/participants/search?q=";
-////        System.out.println("test() before response");
-        HttpResponse<String> response = viewsUnirest.sendUnirestGetRequestGetStringResponse(URL);
-////        System.out.println("test() after response");
-////        System.out.println(response.getBody());
-//
-//        JSONObject body = new JSONObject(response.getBody());
-////        System.out.println("body: " + body);
-//        String beginningKey = body.names().getString(0);
-////        System.out.println("beginningKey: " + beginningKey);
-////        System.out.println();
-//
-////        System.out.println("NO INDEX: " + body.names());
-//
-//        JSONObject participants = body.getJSONObject(beginningKey);
-////        System.out.println("participants: " + participants);
-////        String key = participants.names().getString(0);
-////        System.out.println("key: " + key);
-////        System.out.println();
-//
-//        for(Object o : participants.names()) {
-//            JSONObject p1 = participants.getJSONObject(o.toString());
-//            System.out.println("key: " + o.toString() + ", p1: " + p1.getString("Forename"));;
-//        }
-
-        System.out.println(menteeService.getMenteesFromViews().size());
-
-        for(Optional<ViewsMentee> vm : menteeService.getMenteesFromViews()) {
-            if(vm.isPresent()) {
-                System.out.println(vm.get().getParticipantId());
-            }
-        }
-
-        return response.getBody();
     }
 }
 
