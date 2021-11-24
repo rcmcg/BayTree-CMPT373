5971105d8e174ff97255cf1a3d448ea8362ef154
rchawla <rchawla@sfu.ca>
2021-11-23 16:58:22 -0800

    Added the delete method template

--- admin-frontend/src/pages/Resources/api.ts
+++ admin-frontend/src/pages/Resources/api.ts
@@ -17,6 +17,10 @@ class Api{
     static async post<ResponseType, DataType>(url: string, data?:DataType){
         return Api.axiosInstance.post<DataType,AxiosResponse<ResponseType>>(url, data);
     }
+
+    static async delete<ResponseType, DataType = undefined>(url: string) {
+        return await Api.axiosInstance.delete<DataType, AxiosResponse<ResponseType>>(url)
+      }
 }
 
 export default Api;
