2c653345c72f86dced48e146c44f75e93e8a5052
Pruthviraj Patel <pruthviraj_patel@sfu.ca>
2021-11-27 20:14:09 -0800

    Cleaned up Session.java

--- api/baytree_mentoring/src/main/java/com/baytree_mentoring/baytree_mentoring/models/Session.java
+++ api/baytree_mentoring/src/main/java/com/baytree_mentoring/baytree_mentoring/models/Session.java
@@ -5,12 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 
 @Entity
 @Getter
@@ -23,16 +22,21 @@ public class Session {
 
     @NotNull
     private long menteeId;
+
     private long mentorId;
+
     private long sessionGroupId;
+
     private boolean didMenteeAttend;
+
     private boolean didMentorAttend;
+
     @NotNull
     private String clockInTimeLocal;
+
     @NotNull
     private String clockOutTimeLocal;
-    private Instant clockInTimeUTC;
-    private Instant clockOutTimeUTC;
+
     private long leadStaffId;
 
     @NotNull
@@ -50,15 +54,6 @@ public class Session {
         this.clockOutTimeLocal = clockOutTimeLocal;
         this.leadStaffId = leadStaffId;
         this.sessionNotes = sessionNotes;
-
-        this.clockInTimeUTC = convertToUTC(clockInTimeLocal);
-        this.clockOutTimeUTC = convertToUTC(clockOutTimeLocal);
-    }
-
-    private Instant convertToUTC(String time) {
-        String pattern = "uuuu-MM-dd HH:mm:ss Z"; //Example: "2021-10-03 10:11:23 -0400"
-        LocalDateTime localDateTime = LocalDateTime.parse(time, DateTimeFormatter.ofPattern(pattern));
-        return localDateTime.atZone(ZoneId.of(time.substring(20))).toInstant();
     }
 }
 
