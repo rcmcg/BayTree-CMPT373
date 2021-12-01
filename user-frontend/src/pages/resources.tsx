d98faff2be0b5560578fcbfef3140706f1ce4ff8
rchawla <rchawla@sfu.ca>
2021-11-30 23:07:08 -0800

    Added the table

--- user-frontend/src/pages/resources.tsx
+++ user-frontend/src/pages/resources.tsx
@@ -1,6 +1,7 @@
 // @flow
-import { Typography } from '@material-ui/core';
 import { Resource } from './types';
+import { getResourcesList, createResource, deleteResource } from './resources.api';
+import { Box, IconButton, makeStyles, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from '@material-ui/core/';
 import React, { FormEventHandler, useEffect, useState } from 'react';
 
 export const Resources = () => {
@@ -8,6 +9,32 @@ export const Resources = () => {
 
     return (<>
     <Typography variant="h3">Resources</Typography>
+    <TableContainer>
+  <Table>
+    <TableHead>
+      <TableRow>
+        <TableCell>ID</TableCell>
+        <TableCell>Resource Name</TableCell>
+        <TableCell>Resource Link</TableCell>
+      </TableRow>
+    </TableHead>
+    <TableBody>
+      {resources && resources.map(resource => (
+        <TableRow>
+          <TableCell>
+            {resource.resourceId}
+          </TableCell>
+          <TableCell>
+            {resource.resourceName}
+          </TableCell>
+          <TableCell>
+            {resource.resourceLink}
+          </TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  </Table>
+  </TableContainer>
     </>
     );
 };
