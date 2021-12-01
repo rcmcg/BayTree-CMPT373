f7082b4855113fbb7f8d2ed22cffb6e3eaadff99
rchawla <rchawla@sfu.ca>
2021-11-30 22:59:26 -0800

    Added the state variable for the resource

--- user-frontend/src/pages/resources.tsx
+++ user-frontend/src/pages/resources.tsx
@@ -1,8 +1,11 @@
 // @flow
 import { Typography } from '@material-ui/core';
-import * as React from 'react';
+import { Resource } from './types';
+import React, { FormEventHandler, useEffect, useState } from 'react';
 
 export const Resources = () => {
+    const [resources, setResources] = useState<Resource[] | null>(null)
+
     return (<>
     <Typography variant="h3">Resources</Typography>
     </>
