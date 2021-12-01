4f5734e1d343cb658c49aececbdaa0153d6bc4af
jimmybui <jimmybuidev@gmail.com>
2021-11-30 18:30:02 -0800

    Getting mentors information

--- admin-frontend/src/pages/Home/Home.tsx
+++ admin-frontend/src/pages/Home/Home.tsx
@@ -1,42 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios, {AxiosError, AxiosResponse} from "axios";
+import {backendApiURL, HTTP_CREATED_STATUS_RESPONSE} from "../../App";
 
-interface IState {
-    mentorsList: [],
+interface mentorsList {
+    viewsId: number,
+    firstName: string,
+    lastName: string
 }
 
-var attendedSessions = "";
-var id;
-
-interface props {
-    mentorsList: [],
-}
-
-class SelectMentor extends React.Component<props> {
-    render () {
-        return (
-            <div>
-                <label form="selectMentorId"> Mentor Name </label>
-                <select id={"selectMentorId"} name={"mentorId"}>
-                    <option value={""}>Select a mentor</option>
-                    {this.props.mentorsList.map(mentor => <option value = {mentor["viewsId"]}>{mentor["firstName"] + " " + mentor["lastName"]}</option>)}
-                </select>
-            </div>
-        )
-    }
-}
-
-const fetchSessions  = async () => {
-    axios.get('http://localhost:8080//sessions/get/views/{id}')
-        .then((res: any) => {
-            if(res.date !== null) {
-                attendedSessions = res.attendance;
-            }
-        })
-        .catch((err: any) => {
-            console.log(err);
-        })
-}
+// const fetchSessions  = async () => {
+//     axios.get('http://localhost:8080/sessions/get/views/{id}')
+//         .then((res: any) => {
+//             if(res.date !== null) {
+//                 attendedSessions = res.attendance;
+//             }
+//         })
+//         .catch((err: any) => {
+//             console.log(err);
+//         })
+// }
 
 class SessionInfo {
     render() {
@@ -46,30 +28,46 @@ class SessionInfo {
     }
 }
 
-export class Home extends React.Component<{}, IState>  {
+function Home() {
+    const [mentors, setMentors] = useState<mentorsList[]>([]);
 
-    componentDidMount() {
-        axios.get('http://localhost:8080//user/get/mentors/all')
-            .then((res: any) => {
-                this.setState({ mentorsList : res.data });
-                id = res.viewsId;
+    const fetchMentor = async () => {
+
+        await axios.get<mentorsList[]>('http://localhost:8080/user/get/mentors/all')
+            .then((response) => {
+                const mentorDB = response.data;
+                setMentors([])
+                mentorDB.map(mentor => {
+                    mentors.push(mentor);
+
+                })
             })
-            .catch((err: any) => {
-                console.log(err);
-            })
+        console.log(mentors);
     }
 
-    render() {
-        return (
-            <div className='home'>
-                <h1>Home</h1> <br/>
-                <SelectMentor mentorsList={this.state.mentorsList} /> <br/>
-                <div>
+    useEffect (() => {
+        fetchMentor();
+    }, []);
 
-                </div>
+    return (
+        <div className='home'>
+            <h1>Home</h1> <br/>
+
+            {console.log("HERE")}
+            {console.log(mentors)}
+
+            <div>
+                <label form="selectMentorId"> Mentor Name </label>
+                <select id={"selectMentorId"} name={"mentorId"}>
+                    <option value={""}>Select a mentor</option>
+                    {mentors.map(mentor => <option value = {mentor["viewsId"]}>{mentor["firstName"] + " " + mentor["lastName"]}</option>)}
+                </select>
             </div>
-        );
-    }
+
+            <br/>
+        </div>
+    );
 }
 
 export default Home;
+
