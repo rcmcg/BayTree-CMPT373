f0955bd5af09c31f12f0766853a76616f9624724
rchawla <rchawla@sfu.ca>
2021-11-30 22:51:19 -0800

    initiated the resources page

--- user-frontend/src/pages/resources.tsx
+++ user-frontend/src/pages/resources.tsx
@@ -1,13 +1,10 @@
 // @flow
+import { Typography } from '@material-ui/core';
 import * as React from 'react';
 
 export const Resources = () => {
-    return (
-        <div style={{display:"flex",
-            justifyContent: "center",
-            alignItems:"center",
-            height: "90vh"}}>
-            <h1> Resources</h1>
-        </div>
+    return (<>
+    <Typography variant="h3">Resources</Typography>
+    </>
     );
 };
